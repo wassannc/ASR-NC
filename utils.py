2246c0b2314a3586c65eefa6fd96df0a7f9cd17e
wassannc <balaram@wassan.org>
2026-06-03 12:59:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,15 +10,8 @@ PROJECT_ID = st.secrets["PROJECT_ID"]
 @st.cache_data(ttl=300)
 def load_data(form_id):
     url = f"{ODK_URL}/v1/projects/{PROJECT_ID}/forms/{form_id}/submissions.csv"
-    st.write("ODK_URL =", repr(ODK_URL))
-    st.write("PROJECT_ID =", repr(PROJECT_ID))
-    st.write("FORM_ID =", repr(form_id))
-    st.write("URL =", url)
+    
     response = requests.get(url, auth=(USERNAME, PASSWORD))
-
-    st.write("STATUS =", response.status_code)
-    st.write("CONTENT TYPE =", response.headers.get("content-type"))
-    st.write(response.text[:300])
 
     if response.status_code != 200:
         st.error(f"Error: {response.status_code}")
