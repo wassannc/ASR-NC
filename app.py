d560c037b2eff54a20de6e43764247e5abb7bfce
wassannc <balaram@wassan.org>
2026-06-10 11:35:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,19 +123,19 @@ elif page in FORMS:
             
         # If Crop Model is None/blank, combine Main Crop + Crop Type
         if (
-            "plot_reg.crop_model" in df.columns and
-            "plot_reg.main_crop" in df.columns and
-            "plot_reg.crop_type" in df.columns
+            "plot_reg-crop_model" in df.columns and
+            "plot_reg-main_crop" in df.columns and
+            "plot_reg-crop_type" in df.columns
         ):
             mask = (
-                df["plot_reg.crop_model"].isna() |
-                (df["plot_reg.crop_model"] == "") |
-                (df["plot_reg.crop_model"] == "None")
+                df["plot_reg-crop_model"].isna() |
+                (df["plot_reg-crop_model"] == "") |
+                (df["plot_reg-crop_model"] == "None")
             )
-            df.loc[mask, "Crop Model Final"] = (
-                df["plot_reg.main_crop"].fillna("") +
+            df.loc[mask, "plot_reg-crop_model"] = (
+                df["plot_reg-main_crop"].fillna("") +
                 " - " +
-                df["plot_reg.crop_type"].fillna("")
+                df["plot_reg-crop_type"].fillna("")
             )
                                                 
     if df.empty:
