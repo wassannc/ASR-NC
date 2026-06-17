cbdae02cef72a80e80b86e612a021601e0a45434
wassannc <balaram@wassan.org>
2026-06-17 17:01:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -128,6 +128,18 @@ if page == "Dashboard":
             f"₹{enterprise_income:,.0f}"
         )
     st.stop()
+st.markdown("---")
+st.subheader("🌾 Crop-wise Farmers")
+crop_summary = (
+    nf_df.groupby("plot_reg-main_crop")
+    .size()
+    .reset_index(name="Farmers")
+    .sort_values("Farmers", ascending=False)
+)
+
+st.bar_chart(
+    crop_summary.set_index("plot_reg-main_crop")
+)
 if page == "MIS-Status":
     import pandas as pd
     import calendar
