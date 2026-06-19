080d7cd5162a0fcf7a5a233eb2aa7f60d44c6dff
wassannc <balaram@wassan.org>
2026-06-19 17:24:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,25 +53,6 @@ if page == "Dashboard":
     nf_df = load_data(
         FORMS["1.NF- Register"]["form_id"]
     )
-    activity_df = load_data(
-        FORMS["1.1 NF- Activities"]["form_id"]
-    )
-
-    cb_df = load_data(
-        FORMS["6.Capacity Building"]["form_id"]
-    )
-
-    brc_df = load_data(
-        FORMS["2.Bio Resource Centers"]["form_id"]
-    )
-
-    livestock_df = load_data(
-        FORMS["3.Livestock"]["form_id"]
-    )
-
-    me_df = load_data(
-        FORMS["5.Micro Enterprizes"]["form_id"]
-    )
     
     block_col = "plot_reg-block"
 
